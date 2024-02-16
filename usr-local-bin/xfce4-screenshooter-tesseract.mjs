5f16084234b5193e0b31d081a8b268b551513976
5-tom <tom5@posteo.net>
2024-02-16 10:58:15 +0000

    zx version of xfce4-screenshooter-tesseract

---
+++ usr-local-bin/xfce4-screenshooter-tesseract.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env zx
+
+// xfce4-screenshooter -r -o ./xfce4-screenshooter-tesseract.mjs
+
+const a = await $`mktemp`;
+await $`tesseract ${argv._[0]} ${a} 2>/dev/null`;
+await $`mousepad ${a}.txt`;
