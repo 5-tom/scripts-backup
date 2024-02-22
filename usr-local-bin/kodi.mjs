c2525ad0095e033c02555f1aa63642a11d063b87
5-tom <tom5@posteo.net>
2024-02-22 10:41:34 +0000

    Switch statement for destination folder

--- usr-local-bin/kodi.mjs
+++ usr-local-bin/kodi.mjs
@@ -3,32 +3,55 @@
 import inquirer from "inquirer";
 import { extname, basename, dirname } from "node:path";
 
-const files = (
-	await $`find . -name "*.mp4" -o -name "*.mkv" -o -name "*.srt" | fzf -m --prompt="Select the files you want to rename (Press Esc to skip): "`
-).stdout
-	.replace(/\n$/, "")
-	.split("\n");
+// const files = (
+// 	await $`find . -name "*.mp4" -o -name "*.mkv" -o -name "*.srt" | fzf -m --prompt="Select the files you want to rename (Press Esc to skip): "`
+// ).stdout
+// 	.replace(/\n$/, "")
+// 	.split("\n");
 
-const answers = await inquirer.prompt(
-	files.map(function (file, index) {
-		return {
-			name: String(index),
-			message: `Enter a new filename for ${basename(
-				file
-			)} (Press Enter to skip):`
-		};
-	})
-);
+// const answers = await inquirer.prompt(
+// 	files.map(function (file, index) {
+// 		return {
+// 			name: String(index),
+// 			message: `Enter a new filename for ${basename(
+// 				file
+// 			)} (Press Enter to skip):`
+// 		};
+// 	})
+// );
 
-const move = files.map(function (oldPath, index) {
-	return {
-		oldPath,
-		newPath: `${dirname(oldPath)}/${answers[index]}${extname(oldPath)}`
-	};
-});
+// const move = files.map(function (oldPath, index) {
+// 	return {
+// 		oldPath,
+// 		newPath: `${dirname(oldPath)}/${answers[index]}${extname(oldPath)}`
+// 	};
+// });
 
-move.forEach(async function (paths) {
-	await $`mv ${paths.oldPath} ${paths.newPath}`;
-});
+// move.forEach(async function (paths) {
+// 	await $`mv ${paths.oldPath} ${paths.newPath}`;
+// });
 
-console.log(move);
+const dir = (
+	await inquirer.prompt([
+		{
+			name: "dir",
+			message:
+				"Is your destination folder in /home or /media? (Type either home or media):"
+		}
+	])
+).dir;
+
+let outDir;
+switch (dir) {
+	case "home":
+		outDir = $`find /home -type d | fzf --prompt="Select destination folder: "`;
+		break;
+	case "media":
+		outDir = $`find /media -type d | fzf --prompt="Select destination folder: "`;
+		break;
+	default:
+		console.log("You can only pick home or media. Exiting...");
+		process.exit(1);
+}
+
+console.log(dir);
