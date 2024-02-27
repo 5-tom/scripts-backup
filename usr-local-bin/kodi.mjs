13ada578bbb6ccac9c5160cbef3e3b763cb3e61b
5-tom <tom5@posteo.net>
2024-02-27 19:13:23 +0000

    zx version of kodi

--- usr-local-bin/kodi.mjs
+++ usr-local-bin/kodi.mjs
@@ -3,33 +3,33 @@
 import inquirer from "inquirer";
 import { extname, basename, dirname } from "node:path";
 
-// const files = (
-// 	await $`find . -name "*.mp4" -o -name "*.mkv" -o -name "*.srt" | fzf -m --prompt="Select the files you want to rename (Press Esc to skip): "`
-// ).stdout
-// 	.replace(/\n$/, "")
-// 	.split("\n");
+const files = (
+	await $`find . -name "*.mp4" -o -name "*.mkv" -o -name "*.srt" | fzf -m --prompt="Select the files you want to rename (Press Esc to skip): "`
+).stdout
+	.replace(/\n$/, "")
+	.split("\n");
 
-// const answers = await inquirer.prompt(
-// 	files.map(function (file, index) {
-// 		return {
-// 			name: String(index),
-// 			message: `Enter a new filename for ${basename(
-// 				file
-// 			)} (Press Enter to skip):`
-// 		};
-// 	})
-// );
+const answers = await inquirer.prompt(
+	files.map(function (file, index) {
+		return {
+			name: String(index),
+			message: `Enter a new filename for ${basename(
+				file
+			)} (Press Enter to skip):`
+		};
+	})
+);
 
-// const move = files.map(function (oldPath, index) {
-// 	return {
-// 		oldPath,
-// 		newPath: `${dirname(oldPath)}/${answers[index]}${extname(oldPath)}`
-// 	};
-// });
+const move = files.map(function (oldPath, index) {
+	return {
+		oldPath,
+		newPath: `${dirname(oldPath)}/${answers[index]}${extname(oldPath)}`
+	};
+});
 
-// move.forEach(async function (paths) {
-// 	await $`mv ${paths.oldPath} ${paths.newPath}`;
-// });
+move.forEach(async function (paths) {
+	await $`mv ${paths.oldPath} ${paths.newPath}`;
+});
 
 const dir = (
 	await inquirer.prompt([
@@ -44,14 +44,39 @@ const dir = (
 let outDir;
 switch (dir) {
 	case "home":
-		outDir = $`find /home -type d | fzf --prompt="Select destination folder: "`;
+		outDir = (
+			await $`find /home -path "$HOME/.local" -prune -o -type d -print | fzf --prompt="Select destination folder: "`
+		).stdout.replace(/\n$/, "");
 		break;
 	case "media":
-		outDir = $`find /media -type d | fzf --prompt="Select destination folder: "`;
+		outDir = (
+			await $`find /media -type d | fzf --prompt="Select destination folder: "`
+		).stdout.replace(/\n$/, "");
 		break;
 	default:
 		console.log("You can only pick home or media. Exiting...");
 		process.exit(1);
 }
 
-console.log(dir);
+const filename0 = (
+	await $`find . -name "*.mp4" -o -name "*.mkv" -o -name "*.srt" | fzf -m --prompt="Select the files you want to move: "`
+).stdout
+	.replace(/\n$/, "")
+	.split("\n");
+
+const move2 = filename0.map(function (oldPath) {
+	return {
+		oldPath,
+		newPath: `${outDir}/${basename(oldPath, extname(oldPath))}/${basename(
+			oldPath
+		)}`
+	};
+});
+
+move2.forEach(async function (paths) {
+	console.log(`Moving file:\n${basename(paths.oldPath)}`);
+	await $`mkdir ${dirname(paths.newPath)}`;
+	await $`cp -i ${paths.oldPath} ${paths.newPath}`;
+});
+
+console.log("Complete.");
