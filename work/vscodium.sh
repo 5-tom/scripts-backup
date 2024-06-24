0091faf73b62ff9aa8e2b69db88bf8353a9b0d7e
5-tom <tom5@posteo.net>
2024-06-24 14:30:47 +0100

    Version lens vscodium extension

--- work/vscodium.sh
+++ work/vscodium.sh
@@ -5,3 +5,4 @@ cd $HOME
 # /snap/bin/codium --install-extension ms-dotnettools.csdevkit --force
 # /snap/bin/codium --install-extension ms-dotnettools.vscode-dotnet-runtime --force
 # /snap/bin/codium --install-extension prisma.prisma --force
+# /snap/bin/codium --install-extension pflannery.vscode-versionlens --force
